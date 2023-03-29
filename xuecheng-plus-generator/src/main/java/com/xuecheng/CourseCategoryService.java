fdccb54bee342ca336fa928843a4c5c419bcfe95
YJL <1797123777@qq.com>
2023-03-29 23:33:49 +0800

    课程查询，分类查询，新增接口实现与测试；前后端联调并解决跨域问题；统一异常处理以及自定义异常处理；

--- xuecheng-plus-generator/src/main/java/com/xuecheng/CourseCategoryService.java
+++ xuecheng-plus-generator/src/main/java/com/xuecheng/CourseCategoryService.java
@@ -1,4 +1,4 @@
-package com.xuecheng.content.service;
+package com.xuecheng;
 
 import com.xuecheng.content.model.po.CourseCategory;
 import com.baomidou.mybatisplus.extension.service.IService;
--- xuecheng-plus-generator/src/main/java/com/xuecheng/CourseTeacherService.java
+++ xuecheng-plus-generator/src/main/java/com/xuecheng/CourseTeacherService.java
@@ -1,4 +1,4 @@
-package com.xuecheng.content.service;
+package com.xuecheng;
 
 import com.xuecheng.content.model.po.CourseTeacher;
 import com.baomidou.mybatisplus.extension.service.IService;
